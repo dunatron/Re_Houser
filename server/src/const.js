4c758d0feeb4fc514d3fb56777b6398ecec89dba
Heath Dunlop <heathd@f320m-66139.op.ac.nz>
2019-12-15 21:37:05 +1300

    ff

--- server/src/const.js
+++ server/src/const.js
@@ -1,4 +1,5 @@
 // const JWT_TOKEN_MAX_AGE = 1000 * 60 * 2; // 2 minutes
-const JWT_TOKEN_MAX_AGE = 1000 * 60 * 5; // 5 minutes
+// const JWT_TOKEN_MAX_AGE = 1000 * 60 * 5; // 5 minutes
+const JWT_TOKEN_MAX_AGE = 1000 * 60 * 60 * 24 * 365; // 5 minutes
 // maxAge: 1000 * 60 * 60 * 24 * 365
 module.exports.JWT_TOKEN_MAX_AGE = JWT_TOKEN_MAX_AGE;
