68da8f4b2eb6b55f7f5f6b1cba91bc5422718072
Dunatron <heath.dunlop.hd@gmail.com>
2020-02-10 20:10:25 +1300

    ff

--- client/cypress-tests/cypress/plugins/index.js
+++ client/cypress-tests/cypress/plugins/index.js
@@ -1,21 +1,25 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const fs = require('fs');
+const path = require('path');
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
+
+module.exports = (on, config) => {
+  on('task', {
+    getSchema() {
+      // return fs.readFileSync(
+      //   path.resolve(__dirname, '../../../../server/src/schema.graphql'),
+      //   'utf8'
+      // );
+      return fs.readFileSync(
+        path.resolve(
+          __dirname,
+          '../../../../server/src/generated/prisma.graphql'
+        ),
+        'utf8'
+      );
+    },
+  });
+};
--- client/cypress-tests/cypress/support/commands.js
+++ client/cypress-tests/cypress/support/commands.js
@@ -1,3 +1,5 @@
+import 'cypress-graphql-mock';
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -24,13 +26,13 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 Cypress.Commands.add('clickRecaptcha', () => {
+  // I believe we can setup the recaptcha stuff here and then do an execute??
   //   cy.window().then(win => {
   //     win.document
   //       .querySelector("iframe[src*='recaptcha']")
   //       .contentDocument.getElementById('recaptcha-token')
   //       .click();
   //   });
-  cy.get('[data-cy=submit-login]').click();
 });
 Cypress.Commands.add('register', (email, password) => {});
 Cypress.Commands.add('login', (email, password) => {
@@ -39,7 +41,7 @@ Cypress.Commands.add('login', (email, password) => {
   cy.get('[data-cy=email]').type(Cypress.env('USER_USERNAME'));
   cy.get('[data-cy=password]').type(Cypress.env('USER_USERNAME'));
   cy.clickRecaptcha();
-
+  cy.get('[data-cy=submit-login]').click();
   //   cy.get('[data-cy=submit]').click()
   //   sign-in-tab
 });
