8ec0cb7b003889bf1413bd36f4e301dce509f77c
Dunatron <heath.dunlop.hd@gmail.com>
2020-03-20 02:40:14 +1300

    ff

--- server/src/resolvers/mutations/signout.js
+++ server/src/resolvers/mutations/signout.js
@@ -1,6 +1,12 @@
 async function signout(parent, args, ctx, info) {
-  ctx.response.clearCookie("token");
-  ctx.response.clearCookie("refresh-token");
+  ctx.response.clearCookie("token", {
+    sameSite: "None",
+    secure: true
+  });
+  ctx.response.clearCookie("refresh-token", {
+    sameSite: "None",
+    secure: true
+  });
   return { message: "Goodbye!" };
 }
 
