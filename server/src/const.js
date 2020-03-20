1cf2cf60817f8c14fee2e4f5f90a5b7c74527fb5
Heath Dunlop <heathd@f320m-66139.op.ac.nz>
2020-03-20 16:10:15 +1300

    ff

--- server/src/const.js
+++ server/src/const.js
@@ -13,10 +13,10 @@ exports.rehouserCookieOpt = () => {
       httpOnly: true
     };
 
-  return {
-    maxAge: JWT_TOKEN_MAX_AGE,
-    httpOnly: true
-  };
+  // return {
+  //   maxAge: JWT_TOKEN_MAX_AGE,
+  //   httpOnly: true
+  // };
 
   // use this if you fuck up ypur environment variables and cannot be fucked debugging the cunt
   return {
