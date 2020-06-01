98243c6c9514365842aac7f5afc621b87b257b1b
Dunatron <heath.dunlop.hd@gmail.com>
2020-06-01 15:00:28 +1200

    ff

--- client/lib/configs/propertyAppraisalForm.js
+++ client/lib/configs/propertyAppraisalForm.js
@@ -89,36 +89,14 @@ const PROPERTY_APPRAISAL_CONF = [
             <p>I authorise the Landlord/Property Manager to:</p>
             <ul>
               <li>
-                collect, retain and use this information for the purpose of
-                assessing my creditworthiness and suitability for the tenancy
+                Collect, retain and use this information for the purpose of
+                assessing a fair rental appraisal.
               </li>
               <li>
-                disclose information about me, whether collected from me
-                directly or from any other source, to any other credit provider
-                or any credit reporting agency for the purposes of providing or
-                obtaining a credit report (which will involve the credit
-                reporting agency providing information about me to the
-                Landlord/Property Manager)
+                Use this information to contact you in relation to creating new
+                business relationships in relation to Property Managament.
               </li>
             </ul>
-            <p>I understand that the credit reporting agency: </p>
-            <ul>
-              <li>
-                may hold my information on their credit reporting database and
-                use it for providing credit reporting services, and they may
-                disclose my information to their subscribers for the purpose of
-                credit checking or debt collection
-              </li>
-              <li>
-                as part of providing a credit report, may check the Ministry of
-                Justice fines database for any overdue fines I may have.
-              </li>
-            </ul>
-            <p>
-              Under the Privacy Act 1993, you have the right to ask for a copy
-              of all information held about you, and have the right to request
-              the correction of any incorrect information
-            </p>
           </div>
         ),
       },
@@ -131,12 +109,12 @@ const PROPERTY_APPRAISAL_CONF = [
           // defaultValue: moment().format('YYYY-MM-DDTkk:mm'),
         },
         terms:
-          'I declare That I have read all the terms and stuff. We need to be clear as day with this.',
+          'Under the Privacy Act 1993, you have the right to ask for a copy of all information held about you, and have the right to request the correction of any incorrect information.',
         refConf: {
           required: {
             value: true,
             message:
-              'You must declare that the information you have supplied is correct',
+              'You must declare that the information you have supplied is correct and you have read the terms and conditions',
           },
           validate: value => value === true,
         },
