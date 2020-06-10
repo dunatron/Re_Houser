59ed8bee80dc2829d1fc5e75e37c7fac216faa40
Dunatron <heath.dunlop@op.ac.nz>
2020-06-10 16:27:20 +1200

    ff

--- server/src/lib/emails/offerRentalAppraisalEmail.js
+++ server/src/lib/emails/offerRentalAppraisalEmail.js
@@ -22,6 +22,12 @@ const updatePropertyLeaseEmail = async function({ toEmail, appraisal, ctx }) {
     html: makeANiceEmail(`We have appraised the property at a weekly rent of $${rent}! \n
     You can create a property at the following link <a href="${process.env.EMAIL_PREFIX}/properties/add?eData=${strJSON}">add Property</a> \n
     It will prefill in the information with the appraised information. You can always edit this information
+<div style="line-height: 18px;">
+    Thank you for giving us the opportunity to provide you with a professional rental appraisal for your property.
+</div>
+<div style="line-height: 18px;">
+    we will get back to you as soon as we have been able to accurately assess your property relative to the market
+</div>
   \n\n`)
     // html: makeANiceEmail(`We have appraised the property at a weekly rent of $${rent}! \n
     //   You can create a property at the following link ${process.env.EMAIL_PREFIX}/properties/add?eData=${strJSON} \n
