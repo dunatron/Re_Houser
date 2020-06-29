f440deccc896a9f51dd5290af4f31856b074385b
Dunatron <heath.dunlop@op.ac.nz>
2020-06-29 15:30:18 +1200

    ff

--- client/components/Page/useStyles.js
+++ client/components/Page/useStyles.js
@@ -13,17 +13,17 @@ const useStyles = makeStyles(theme => {
     },
 
     // MAYBE THIS FFS
-    // appBar: {
-    //   backgroundColor: props =>
-    //     props.noTransparency
-    //       ? theme.overrides.MuiAppBar
-    //         ? `${theme.overrides.MuiAppBar.colorPrimary.backgroundColor} !important`
-    //         : `${theme.palette.background.paper} !important`
-    //       : 'transparent',
-    //   [theme.breakpoints.up('lg')]: {
-    //     width: `calc(100% - ${theme.sideBarWidth}px)`,
-    //   },
-    // },
+    appBar: {
+      backgroundColor: props =>
+        props.noTransparency
+          ? theme.overrides.MuiAppBar
+            ? `${theme.overrides.MuiAppBar.colorPrimary.backgroundColor} !important`
+            : `${theme.palette.background.paper} !important`
+          : 'transparent !important',
+      [theme.breakpoints.up('lg')]: {
+        width: `calc(100% - ${theme.sideBarWidth}px)`,
+      },
+    },
     menuButton: {
       marginLeft: 0,
       [theme.breakpoints.up('lg')]: {
