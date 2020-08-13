4c301223840975a4cddf05a4bccdeb6f3ae2b944
Dunatron <heath.dunlop@op.ac.nz>
2020-08-13 16:39:29 +1200

    ff

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,76 +1,41 @@
-import React, { Component } from 'react';
-import Banner from '../components/Banner';
+import React, { useState } from 'react';
+import { GiftedChat, IMessage, User } from 'react-native-gifted-chat';
+import { View, Dimensions } from 'react-native';
 
-import { Button, Typography, IconButton } from '@material-ui/core';
-import ChangeRouteButton from '../components/Routes/ChangeRouteButton';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-
-import Dashboard from '../components/Dashboard';
-
-import ContactForm from '../components/Contact/ContactForm';
-import Fees from '../components/Fees';
-import TeamInfoText from '../components/Team/TeamInfoText';
-import Team from '../components/Team/index';
-
-import HOME_PAGE_DASHBOARD_CONFIG from '../lib/configs/homepageDashboardConf';
-
-const HomePage = props => {
-  const {
-    appData: { currentUser },
-  } = props;
-  // query={{ id: data.createProperty.id }}
+const App = () => {
+  const [messages, setMessages] = useState([
+    {
+      _id: 123,
+      text:
+        'This is a test of Gifted Chat for the WEB 🎉 \n https://github.com/FaridSafi/react-native-gifted-chat ',
+      user: {
+        _id: 2,
+        name: 'you',
+        avatar: '/me.jpg',
+      },
+      createdAt: new Date(),
+    },
+    {
+      _id: 456,
+      text:
+        'Find source code here: \n https://github.com/xcarpentier/gifted-chat-web-demo',
+      user: {
+        _id: 2,
+        name: 'you',
+        avatar: '/me.jpg',
+      },
+      createdAt: new Date(),
+    },
+  ]);
+  const onSend = () => setMessages([...messages, ...newMsg]);
+  const user = { _id: 1, name: 'me' };
+  const inverted = false;
+  const { width, height } = Dimensions.get('window');
   return (
-    <div>
-      <Banner
-        imageSrc="images/banners/home-page-banner.jpg"
-        footer={[
-          <Typography variant="h5" style={{ textAlign: 'center' }}>
-            Turning empty houses into friendly abodes
-          </Typography>,
-        ]}>
-        <Typography
-          variant="h4"
-          style={{ marginBottom: '64px', textAlign: 'center' }}>
-          Welcome to Rehouser
-        </Typography>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            height: '112px',
-          }}>
-          <ChangeRouteButton
-            title="Free Appraisal"
-            s
-            variant="contained"
-            color="secondary"
-            route="/freeappraisal"
-          />
-          <ChangeRouteButton
-            title="Search for rental"
-            variant="contained"
-            color="primary"
-            route="/property-search"
-          />
-        </div>
-        <Typography
-          variant="h6"
-          style={{ marginTop: '64px', textAlign: 'center' }}>
-          Read More
-        </Typography>
-        <IconButton aria-label="read-more" href="#banner-footer">
-          <ArrowDownwardIcon fontSize="large" />
-        </IconButton>
-      </Banner>
-      <Dashboard
-        config={HOME_PAGE_DASHBOARD_CONFIG}
-        elevation={0}
-        heading=""
-        intro=""
-      />
-    </div>
+    <View style={{ width, height }}>
+      <GiftedChat {...{ messages, onSend, user, inverted }} />
+    </View>
   );
 };
 
-export default HomePage;
+export default App;
