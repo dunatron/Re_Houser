173194d74f92a7088e08780d3a9023d605960fe8
Dunatron <heath.dunlop@op.ac.nz>
2020-09-07 09:36:11 +1200

    ff

--- server/src/resolvers/querys/propertyFiles.js
+++ server/src/resolvers/querys/propertyFiles.js
@@ -1,10 +1,6 @@
 //PropertyFiles
 async function propertyFiles(parent, args, ctx, info) {
-  console.log("AHhhyhy");
   const associatedFiles = await ctx.db.query.propertyFiles({ ...args }, info);
-
-  console.log("associatedFiles => ", associatedFiles);
-
   return associatedFiles;
 }
 
