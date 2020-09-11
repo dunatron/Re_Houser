40d44416bda1d7514a2f2b12598bdba74c0dfd93
Dunatron <heath.dunlop@op.ac.nz>
2020-09-11 16:52:39 +1200

    ff

--- client/components/Page/useStyles.js
+++ client/components/Page/useStyles.js
@@ -68,7 +68,8 @@ const useStyles = makeStyles(theme => {
     },
     drawerPaper: {
       width: theme.sideBarWidth,
-      backgroundColor: lighten(theme.palette.secondary.light, 0.7), // lighten by 50%. 1.0 is white
+      backgroundColor: 'rgb(212,220,231)',
+      // backgroundColor: lighten(theme.palette.secondary.light, 0.7), // lighten by 50%. 1.0 is white
       // backgroundColor: theme.palette.secondary.light,
       // backgroundColor: theme.palette.secondary.light,
     },
