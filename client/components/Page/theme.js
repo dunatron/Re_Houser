15475c17f6d01bbe13d8474d7776eb2cf553bd5e
Dunatron <heath.dunlop@op.ac.nz>
2020-09-29 14:26:10 +1300

    ff

--- client/components/Page/theme.js
+++ client/components/Page/theme.js
@@ -3,6 +3,8 @@ import themeTypography from '@/Styles/_themeTypography';
 import muiTheme from '@/Styles/_muiTheme';
 import { createMuiTheme } from '@material-ui/core/styles';
 
+//https://www.58bits.com/blog/2020/05/27/material-ui-theme-switcher-react
+//https://egghead.io/lessons/react-create-a-react-hook-to-toggle-between-light-and-dark-mode-inside-of-material-ui-themes
 const theme = createMuiTheme({
   ...muiTheme,
   palette: {
