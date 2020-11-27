1de6c6e37d79589b77418527198792bf11cca1cd
Dunatron <heath.dunlop@op.ac.nz>
2020-11-27 16:18:05 +1300

    ff

--- client/components/Signout/index.js
+++ client/components/Signout/index.js
@@ -8,6 +8,7 @@ import { CURRENT_USER_QUERY } from '@/Components/User/index';
 import NavButton from '@/Styles/NavButton';
 import Error from '@/Components/ErrorMessage/index';
 import { toast } from 'react-toastify';
+import { parseCookies, setCookie, destroyCookie } from 'nookies';
 
 const SIGN_OUT_MUTATION = gql`
   mutation SIGN_OUT_MUTATION {
@@ -21,6 +22,7 @@ const Signout = ({ label, fullWidth, me, color, variant }) => {
   const client = useApolloClient();
 
   const handleCompleted = data => {
+    destroyCookie(null, 'token');
     client.resetStore();
     toast.success(
       <p>
@@ -31,7 +33,7 @@ const Signout = ({ label, fullWidth, me, color, variant }) => {
   const handleOnError = error => {};
 
   const [signout, { data, loading, error }] = useMutation(SIGN_OUT_MUTATION, {
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
     onCompleted: handleCompleted,
     onError: handleOnError,
   });
@@ -59,6 +61,6 @@ Signout.propTypes = {
   fullWidth: PropTypes.any,
   label: PropTypes.any,
   me: PropTypes.any,
-  variant: PropTypes.any
+  variant: PropTypes.any,
 };
 export default Signout;
